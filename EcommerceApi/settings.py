a7cc395d4147159c216c7fdb41bd19f3968dfbcd
Abednego Emonena <emonenaabednego@gmail.com>
2023-04-03 09:04:08 +0500

    created the django project (ecommerceapi)

--- EcommerceApi/settings.py
+++ EcommerceApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ari-b=mny45e2^-l-m$)u))d8a!^b5v3ilwi_o+=-p&q-6*1&6'
+SECRET_KEY = 'django-insecure-)2qc1xx4&1ma#k2ey(_1h%%_csyg1&x%ef8%7_e$g2gjpfx%sw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
